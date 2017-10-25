4aa21710b1ed79348e1c80c988589a21337bd891
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-10-25 10:00:53 -0700

    Started multiplication table

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -6,5 +6,7 @@
 //******************************
 
 public class MultiplicationTable {
-    public static void main
+    public static void main (String[] args) {
+        System.out.print (" 
+    }
 }
